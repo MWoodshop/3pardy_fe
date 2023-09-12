3270792d7f163b717438cfce6002e49ead81e5a9
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-09-12 12:47:36 -0400

    Feat: updates javascript

--- app/javascript/game.js
+++ app/javascript/game.js
@@ -1,17 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll(".card");
+  const cards = document.querySelectorAll(".card");
+  const cardContainer = document.querySelector(".card-container");
 
-    cards.forEach((card) => {
-      const userInput = card.querySelector('.user-input');
-      userInput.style.display = 'none'; // Initially hide the input box and button
+  cards.forEach((card) => {
+    const userInput = card.querySelector('.user-input');
+    userInput.style.display = 'none'; // Initially hide the input box and button
 
-      card.addEventListener("click", function () {
-        card.classList.toggle("flipped");
-        if (card.classList.contains("flipped")) {
-          userInput.style.display = 'block'; // Show the input box and button when card is flipped
-        } else {
-          userInput.style.display = 'none'; // Hide the input box and button when card is unflipped
-        }
-      });
+    card.addEventListener("click", function () {
+
+      // Check if there's another card already expanded
+      const expandedCard = document.querySelector(".card.expanded");
+      if (expandedCard && expandedCard !== card) {
+        expandedCard.classList.remove("flipped", "expanded");
+        expandedCard.style.top = '';
+        expandedCard.style.left = '';
+        expandedCard.querySelector('.user-input').style.display = 'none';
+      }
+
+      card.classList.toggle("flipped");
+
+      if (card.classList.contains("flipped")) {
+        userInput.style.display = 'block';
+
+        // Calculate center position
+        const centerX = (cardContainer.offsetWidth / 2) - (card.offsetWidth / 2);
+        const centerY = (cardContainer.offsetHeight / 2) - (card.offsetHeight / 2);
+
+        // Move the card to the center
+        card.style.top = `${centerY}px`;
+        card.style.left = `${centerX}px`;
+
+        // Add 'expanded' and 'dimmed' classes
+        card.classList.add("expanded");
+        cardContainer.classList.add("dimmed");
+
+      } else {
+        userInput.style.display = 'none';
+
+        // Remove 'expanded' and 'dimmed' classes
+        card.classList.remove("expanded");
+        cardContainer.classList.remove("dimmed");
+
+        // Reset position
+        card.style.top = '';
+        card.style.left = '';
+      }
     });
   });
+});
